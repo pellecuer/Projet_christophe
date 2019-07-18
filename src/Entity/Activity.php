6ede15d5b73db840f5f5634a00d37de5e3f4d852
David PELLECUER <dpellecuer@adws.amiltone.com>
2019-07-18 13:37:29 +0200

    maj

--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ActivityRepository")
@@ -29,6 +30,7 @@ class Activity
     }
     
     /**
+     * @Assert\Range(min=0, max=9)
      * @ORM\Column(type="integer")
      */
     private $rank;
--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -6,8 +6,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
+ * @UniqueEntity(fields="name", message="Cet nom éxiste déjà")
+ * @UniqueEntity(fields="code", message="Cet code éxiste déjà")
  * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
  */
 class Project
@@ -20,12 +24,12 @@ class Project
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $code;   
 
