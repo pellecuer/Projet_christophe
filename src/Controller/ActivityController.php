1581d0fc55c988ecdbf0d3543422fc6e8c2fb225
David PELLECUER <dpellecuer@adws.amiltone.com>
2019-07-23 16:10:31 +0200

    maj

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -303,7 +303,7 @@ class ActivityController extends AbstractController
 
         $response = new Response(json_encode([
             'id' =>  $id,
-            'rank'=> $activity->getDate()->format('m Y')
+            'rank'=> $activity->getPole()->getName()
         ]));
         $response->headers->set('Content-Type', 'application/json');
         return $response;
