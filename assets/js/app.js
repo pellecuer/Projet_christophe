6adae139e8dfb56e2d50ba1e9caf002b8bab6be5
David PELLECUER <dpellecuer@adws.amiltone.com>
2019-07-26 17:06:38 +0200

    ok last july

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,20 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
-require('./collection.js');
-
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+ // any CSS you require will output into a single css file (app.css in this case)
+//  import('@fortawesome/fontawesome-free/css/all.min.css');
+ import('bootstrap/dist/css/bootstrap.css');
+ // require('datatables/media/css/jquery.dataTables.css');
+ import('../css/app.css');
+ 
+ //js
+//  require('@fortawesome/fontawesome-free/js/all.js');
+ require('popper.js');
+ require('bootstrap');
+//  require('./calculate.js');
+// import('./collection.js');
+ // require('datatables');
